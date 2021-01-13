b7cb60dca03d765b77faa89e5fca820468e83335
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-13 22:53:19 +0530

    store all the prime factor into array

--- Array-Problem/PrimeFactor.sh
+++ Array-Problem/PrimeFactor.sh
@@ -1,12 +1,28 @@
 #!/bin/bash -x
 read -p "enter your number for factorial. : " num
-counter=0
-for(( i=1; i<=$num; i++ ))
+count=0
+declare -a primefactor
+for (( counter=2; counter<=$num; counter++ ))
 do
-	if [ $(( num%$i )) -eq 0 ]
+	flag=1
+	if (( $num%$counter == 0 ))
 	then
-		echo "factors of $num is : $i"
-		factor[ ((counter++)) ]=$i
+		if (( counter==2 ))
+		then
+			flag=1
+		fi
+		for (( base=2; base<counter; base++ ))
+		do
+			if (( $counter%$base==0 ))
+			then
+				flag=0
+				break
+			fi
+		done
+		if (( flag==1 ))
+		then
+			primefactor[((count++))]=$counter
+		fi
 	fi
 done
-echo "all factor stored into array: ${factor[@]}"
+echo ${primefactor[@]}
