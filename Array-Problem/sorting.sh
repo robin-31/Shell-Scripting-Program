5e9e3462238933e242ac6043711b1cf5eb833967
“robin-31” <rm.robinmanna01@gmail.com>
2021-01-13 23:17:04 +0530

    by sorting find 2nd largest as well smallest

--- Array-Problem/sorting.sh
+++ Array-Problem/sorting.sh
@@ -1,24 +1,23 @@
 #!/bin/bash -x
-for(( k=1; k<5; k++ ))
+#arr=(1 57 56 99 20 03 02)
+count=0
+for (( i=1; i<=10; i++ ))
 do
-      	randomNumber=$((RANDOM%1000))
-       	arr[((counter))]=$randomNumber
-     	(( counter= $counter + $k ))
+	arr[((count++))]=$((RANDOM%100))
 done
-#arr=(14 02 56 99 20)
-echo "Array in original order"
-echo ${arr[@]}
 length=${#arr[@]}
 for (( i=0; i<$length; i++ ))
 do
-        for (( j=$i; j<$length; j++ ))
-        do
-                if [[ ${arr[$i]} -gt ${arr[$j]} ]]
+	for (( j=$i; j<$length; j++ ))
+	do
+		if [[ ${arr[$i]} -gt ${arr[$j]} ]]
                 then
-                        temp=${arr[$i]}
+			temp=${arr[$i]}
                         arr[$i]=${arr[$j]}
                         arr[$j]=$temp
                 fi
         done
 done
 echo ${arr[@]}
+echo "second lowest number in array ${arr[1]}"
+echo "second highest number in my array ${arr[8]}"
